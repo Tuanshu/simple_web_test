83bba8a351f1e00bdd373c749d0cceaa661a2c70
tuanshu <tuanshu@gmail.com>
2022-12-17 20:45:38 +0800

    OK

---
+++ test.py
@@ -0,0 +1,18 @@
+# https: // hub.docker.com/r/yeasy/simple-web/
+
+# cmd to run:
+# docker run --rm -it -p 80:80 yeasy/simple-web:latest
+# in container, sudo docker run --rm -it -p 80:80 yeasy/simple-web:latest
+
+# to connect in chrome:
+# (local)
+# http://localhost:80/
+
+# (docker to test)
+# http://172.31.46.90:80/
+
+'''
+但https://52.198.44.17:8081/db/taggedart_scrapy/
+看不了
+https://172.31.46.90:8081/db/taggedart_scrapy/
+'''
